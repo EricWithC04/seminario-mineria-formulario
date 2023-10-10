0af5938cdf3cd3f5b4ea41b9afe639d0969089a2
EricWithC04 <negativegame117@gmail.com>
2023-10-10 11:54:37 -0300

    delete relations

--- server/src/config/relations.js
+++ server/src/config/relations.js
@@ -3,27 +3,6 @@ import UserModel from "../models/User.model.js";
 import SongModel from "../models/Song.model.js";
 import GenreModel from "../models/Genre.model.js";
 
-// LocalityModel.belongsTo(DepartamentModel, {
-//     foreignKey: "idDepartament"
-// });
-// DepartamentModel.hasMany(LocalityModel, {
-//     foreignKey: "idDepartament"
-// });
-// DepartamentModel.belongsTo(ProvinceModel, {
-//     foreignKey: "idProvince"
-// });
-// ProvinceModel.hasMany(DepartamentModel, {
-//     foreignKey: "idProvince"
-// })
-// ProvinceModel.belongsTo(CountryModel, {
-//     foreignKey: "idCountry"
-// })
-// CountryModel.hasMany(ProvinceModel, {
-//     foreignKey: "idCountry"
-// })
-
-
-
 UserModel.belongsTo(GenreModel, {
     foreignKey: "idGenre"
 });
