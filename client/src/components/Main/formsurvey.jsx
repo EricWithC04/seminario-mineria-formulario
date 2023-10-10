fb33860f50363d4febe36c9b7951c56407e062be
Rodri.V <veronrodrigo98@gmail.com>
2023-10-10 14:13:29 -0300

    se modifico el

--- client/src/components/Main/formsurvey.jsx
+++ client/src/components/Main/formsurvey.jsx
@@ -2,28 +2,55 @@ import '../../assets/css/survey.css';
 
 export const Formsurvey = () => {
     return (
-        <div class="box">
-            <div class="head">
-                Survey
+        <div className="box container">
+            <div className='row'>
+                <div className='col'>
+                    <div className="head">
+                        Encuesta: Radio EFR
+                    </div>
+                </div>
             </div>
-            <form>
-                <div>
-                    <div>
-                        <p>Full Name</p>
-                        <input type="text" name="First Name" placeholder="First Name" className="name" />
-                        <input type="text" name="Last Name" placeholder="Last Name" class="name" />
+            {/* Nombre, Edad, Localidad, Genero(hombre o mujer), artista favorito, cancion favorita, genero favorito, si es estudiante, trabajador o vago (que no hace nada) */}
+            <form className='m-4'>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='row'>
+                            <label>Nombre completo</label>
+                            <input type="text" name="fullname" placeholder="Nombre" className="name" />
+                        </div>
                     </div>
-                    <div>
-                        <p>Email</p>
-                        <input type="email" name="email" placeholder="example@gmail.com" class="mail" />
+                    <div className="col">
+                        <div className='row'>
+                            <label>Email</label>
+                            <input type="email" name="email" placeholder="example@gmail.com" className="mail" />
+                        </div>
                     </div>
-                    <div>
-                        <p>Message</p>
-                        <p class="m"><input type="text" name="message" class="message" /></p>
+                    <div className="col">
+                        <div className='row'>
+                            <label>Localidad</label>
+                            <input type="email" name="email" placeholder="localidad" className="mail" />
+                        </div>
                     </div>
-                    <div class="boxbutton">
-                        <button>Submit</button>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='row'>
+                            <label htmlFor="">Género</label>
+                            <select className='w-50' name="" id="">
+                                <option className='' value="">Masculino</option>
+                                <option className='' value="">Femenino</option>
+                            </select>
+                        </div>
                     </div>
+                    <div className="col">
+                        <div className='row'>
+                            <label htmlFor="">Artista favorito</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+                </div>
+                <div className="boxbutton">
+                    <button>Submit</button>
                 </div>
             </form >
         </div>
