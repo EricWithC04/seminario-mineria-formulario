1c5dcc4d09da024c8652a4ed2d25690705603c3d
Rodri.V <veronrodrigo98@gmail.com>
2023-10-12 13:23:56 -0300

    se modifico el formulario de la encuesta

--- client/src/components/Main/formsurvey.jsx
+++ client/src/components/Main/formsurvey.jsx
@@ -1,58 +1,76 @@
 import '../../assets/css/survey.css';
+import { useState } from 'react';
 
 export const Formsurvey = () => {
+
+    {/* Nombre, Edad, Localidad, Genero(hombre o mujer), artista favorito, cancion favorita, genero favorito, si es estudiante, trabajador o vago (que no hace nada) */ }
     return (
-        <div className="box container">
-            <div className='row'>
-                <div className='col'>
-                    <div className="head">
-                        Encuesta: Radio EFR
+        <div className="container">
+            <div className='title'>Encuesta: Radio Formosa</div>
+            <form action="">
+                <div className="user-details">
+                    <div className='input-box'>
+                        <span className='details'>Nombre Completo</span>
+                        <input type="text" placeholder='Ingrese su nombre completo' />
+                    </div>
+                    <div className='input-box'>
+                        <span className='details'>Edad</span>
+                        <input type="text" placeholder='Ingrese su edad' />
                     </div>
                 </div>
-            </div>
-            {/* Nombre, Edad, Localidad, Genero(hombre o mujer), artista favorito, cancion favorita, genero favorito, si es estudiante, trabajador o vago (que no hace nada) */}
-            <form className='m-4'>
-                <div className='row'>
-                    <div className='col'>
-                        <div className='row'>
-                            <label>Nombre completo</label>
-                            <input type="text" name="fullname" placeholder="Nombre" className="name" />
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className='row'>
-                            <label>Email</label>
-                            <input type="email" name="email" placeholder="example@gmail.com" className="mail" />
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className='row'>
-                            <label>Localidad</label>
-                            <input type="email" name="email" placeholder="localidad" className="mail" />
-                        </div>
+                <div className='gender-details'>
+                    <input type="radio" name='gander' id="dot-1" />
+                    <input type="radio" name='gander' id="dot-2" />
+                    <input type="radio" name='gander' id="dot-3" />
+                    <span className='gender-title'>Género</span>
+                    <div className='category'>
+                        <label htmlFor="dot-1">
+                            <span className='dot one'></span>
+                            <span className='gender'>Femenino</span>
+                        </label>
+                        <label htmlFor="dot-2">
+                            <span className='dot two'></span>
+                            <span className='gender'>Masculino</span>
+                        </label>
+                        <label htmlFor="dot-3">
+                            <span className='dot three'></span>
+                            <span className='gender'>Prefiero no decirlo</span>
+                        </label>
                     </div>
                 </div>
-                <div className='row'>
-                    <div className='col'>
-                        <div className='row'>
-                            <label htmlFor="">Género</label>
-                            <select className='w-50' name="" id="">
-                                <option className='' value="">Masculino</option>
-                                <option className='' value="">Femenino</option>
-                            </select>
-                        </div>
+                <div className="user-details">
+
+                    <div className='input-box'>
+                        <span className='details'>Artista Favorito</span>
+                        <input type="text" placeholder='Artista' />
                     </div>
-                    <div className="col">
-                        <div className='row'>
-                            <label htmlFor="">Artista favorito</label>
-                            <input type="text" />
-                        </div>
+                    <div className='input-box'>
+                        <span className='details'>Género favorito</span>
+                        <select className='w-100' name="" id="">
+                            <option value=""></option>
+                        </select>
+                    </div>
+                    <div className='input-box'>
+                        <span className='details'>Ocupación</span>
+                        <select className='w-100' name="" id=""></select>
+                    </div>
+                    <div className='input-box'>
+                        <span className='details'>Canción Favorita</span>
+                        <input type="text" placeholder='Canción' />
+                    </div>
+                    <div className='input-box'>
+                        <span className='details'>Email</span>
+                        <input type="text" placeholder='Ingrese su email' />
+                    </div>
+                    <div className='input-box'>
+                        <span className='details'>localidad</span>
+                        <input type="text" placeholder='Ingrese su localidad' />
                     </div>
                 </div>
-                <div className="boxbutton">
-                    <button>Submit</button>
+                <div className='button'>
+                    <button type='submit' className='btn btn-outline-dark'>Enviar</button>
                 </div>
-            </form >
+            </form>
         </div>
     )
 }
